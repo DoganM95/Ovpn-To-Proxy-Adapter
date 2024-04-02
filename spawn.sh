19b482669f4c06260369add14a06ce933c8c045b
DoganM95 <doganmermertas@hotmail.de>
2024-04-03 01:59:55 +0200

    refactor

--- spawn.sh
+++ spawn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vpn_location=$1
+vpn_location=$(trim_extension "$1") # Location with extension ".ovpn" removed
 vpn_provider=$2
 starting_port=$3
 vpn_username=$4
@@ -8,34 +8,33 @@ vpn_password=$5
 container_restart=$6
 network_cidr=$7
 
+ovpn_list=./ovpn_list
+existing_containers=$(docker ps -a --filter "name=haugene-transmission-openvpn" --format {{.Names}})
+
 main() {
-    vpn_location=$(trim_extension "$vpn_location") # Location with extension ".ovpn" removed
     echo "Removing all haugene-transmission-vpn containers with status=created. These are faulty containers and will now be replaced by working ones."
-    # Remove non-functional containers
-    $(docker rm $(docker ps -a --filter "name=haugene-transmission-openvpn" --filter "status=created" -q))
-    FILE=./ovpn_list
-    EXISTING_CONTAINERS=$(docker ps -a --filter "name=haugene-transmission-openvpn" --format {{.Names}})
+    $(docker rm $(docker ps -a --filter "name=haugene-transmission-openvpn" --filter "status=created" -q)) # Remove non-functional containers
     if [[ "list" = "$vpn_location" ]]; then
-        if [ -e "$FILE" ]; then
-            sed -i '/^$/d' $FILE
-            echo "" >>$FILE
-            echo "Found a list with $(wc -l <$FILE) vpn's."
+        if [ -e "$ovpn_list" ]; then
+            sed -i '/^$/d' $ovpn_list
+            echo "" >>$ovpn_list
+            echo "Found a list with $(wc -l <$ovpn_list) vpn's."
             while read line; do
                 line=$(trim_extension "$line")
-                if ! [[ $EXISTING_CONTAINERS =~ $line ]]; then
+                if ! [[ $existing_containers =~ $line ]]; then
                     echo "Creating container for $line"
                     create_container "$line"
                 else
                     echo "Skipping creation of $line. Equally named container already exists."
                 fi
                 echo
-            done <$FILE
+            done <$ovpn_list
         else
             echo "No ovpn_list file found. Exiting."
             return 1
         fi
     else
-        echo "Creating container for $vpn_location"
+        echo "Creating proxy container for location $vpn_location"
         create_container "$vpn_location"
     fi
 }
@@ -60,12 +59,12 @@ create_container() {
         -e "OPENVPN_CONFIG=$vpn_name" \
         -e "WEBPROXY_ENABLED=true" \
         -e "WEBPROXY_PORT=8118" \
-        --name="haugene-transmission-openvpn-$vpn_location" \
+        --name="haugene-transmission-openvpn-proxy-$vpn_location" \
         -p "$starting_port:8118" \
         --restart "$container_restart" \
         haugene/transmission-openvpn:latest
 
-    echo "Port mappe to this container's Web Proxy: $starting_port"
+    echo "Port mapped to this http proxy: $starting_port"
     ((starting_port++))
 }
 
